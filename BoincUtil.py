0c627ce89ae763dee8e80d6b4689ab840b36833e
David Calle <davidcalle9430@gmail.com>
2016-09-04 19:52:02 -0500

    Input file templates

--- BoincUtil.py
+++ BoincUtil.py
@@ -2,13 +2,34 @@ import os
 import subprocess
 import os, os.path as osp
 from uuid import uuid4
+import xml.etree.cElementTree as ET
 
+
+def create_input_template(filename, project_dir, number):
+    xml = """<file_info>
+            <number>%i</number>
+        </file_info>
+        <workunit>
+            <file_ref>
+                <file_number>%i</file_number>
+                <open_name>%s</open_name>
+                <copy_file/>
+            </file_ref>
+            <rsc_fpops_bound>1e12</rsc_fpops_bound>
+            <rsc_fpops_est>1e14</rsc_fpops_est>
+        </workunit>
+    """ % (number, number, filename)
+    fo = open(osp.join(project_dir, "templates", filename), "wb")
+    fo.write(xml)
+    fo.close()
+    return input_file_name
 
 def stage_file(filename, project_dir):
     head, tail = os.path.split(filename)
     name, ext = osp.splitext(tail)
     fullname = name + '_' + uuid4().hex + ext
-    download_path = subprocess.check_output(['bin/dir_hier_path', osp.join(head, fullname)], cwd = project_dir).strip()
+    create_input_template(fullname, project_dir,0) # ojo, esta quemado el cero ahora
+    download_path = subprocess.check_output(['bin/dir_hier_path', fullname], cwd = project_dir).strip()
     subprocess.call(["cp", filename, download_path])
     return fullname
 
@@ -16,5 +37,6 @@ def create_work(appname, work_unit_name, filenames, project_dir):
     arguments = "--appname %s --wu_name %s" % (appname, work_unit_name)
     for filename in filenames:
         real_name = stage_file(filename, project_dir)
+        rguments += "--wu_template %s" % real_name
         arguments += " %s" % real_name
     subprocess.call([project_dir + "/bin/create_work", arguments])
