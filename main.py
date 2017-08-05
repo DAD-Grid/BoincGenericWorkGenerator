582b8a2d142c753da99731afe42c70a833632aed
root <root@e06f5a03fe1f>
2017-08-05 00:10:17 +0000

    adding command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+import sys
 from App import App
 from GenericWorkGenerator import GenericWorkGenerator
 
 if __name__ == '__main__':
-    app = App("gaussianFilter","/home/boincadm/projects/project3")
-    generator = GenericWorkGenerator("bin/generator", "bin/fruit1.png")
-    generator.create_work(app)
-    print "Done!"
+    if len(sys.argv) < 5 :
+	print 'usage: python main.py app_name project_path generator_path image_path'
+    else: 
+	print 'args ',  str(sys.argv)
+    	app = App(sys.argv[1], sys.argv[2])
+    	generator = GenericWorkGenerator(sys.argv[3], sys.argv[4])
+    	generator.create_work(app)
+    	print "Done!"
